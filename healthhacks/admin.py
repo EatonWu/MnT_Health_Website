0e481a68aea029672b4ef45d265b92abee1f494e
Kellen Bell <kdb9520@rit.edu>
2022-02-05 16:27:53 -0500

    Registered the models to the admin sit.

--- healthhacks/admin.py
+++ healthhacks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import User, CalorieCalc, FitnessPlan
+from .models import User, CalorieCalc, FitnessPlan
 
 # Register your models here.
 admin.site.register(User)
