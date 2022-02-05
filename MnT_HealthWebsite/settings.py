151bc5a553dfe1e06abc53424b01511b552b95ec
Kellen Bell <kdb9520@rit.edu>
2022-02-05 16:29:58 -0500

    Updated the installed apps.

--- MnT_HealthWebsite/settings.py
+++ MnT_HealthWebsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MnT_Health_Website.healthhacks.apps.HealthhacksConfig'
 ]
 
 MIDDLEWARE = [
