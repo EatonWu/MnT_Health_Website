ae06f278006fe384a32d08909f68cf019534d8f1
aln4739 <henryy20022@gmail.com>
2022-02-06 11:00:54 -0500

    changed allowed hosts

--- MnT_HealthWebsite/settings.py
+++ MnT_HealthWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^)ipkal(m@v^*dv@#sf)vqk5!8o_j^h-oit!8yu-!kx=pdt%(j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://fierce-thicket-15349.herokuapp.com/"]
+ALLOWED_HOSTS = ["fierce-thicket-15349.herokuapp.com", "127.0.0.1"]
 
 # Application definition
 
