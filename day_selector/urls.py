fdfcd45d4e65f3530755986dd2142bea320c232c
aln4739 <henryy20022@gmail.com>
2022-02-05 21:56:33 -0500

    URLS mapped.

--- day_selector/urls.py
+++ day_selector/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='fitness_index'),
     path('<int:day>/', views.day_selected, name='day_selected')
 ]
